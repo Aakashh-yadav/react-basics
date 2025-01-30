6f2bfa0dae96f32ed8792c8b50b9dd05a9a1bcde
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2025-01-30 09:02:12 +0530

    some more updates in Todo

--- constext_api_with_local_Storage/src/App.jsx
+++ constext_api_with_local_Storage/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import { useState } from "react";
+import TodoForm from "./components/TodoForm";
 import { TodoContext, TodoProvider, useTodoContext } from "./Contexts/Index";
 import { useEffect } from "react";
 function App() {
-  const[todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
   const addtodo = (title) => {
     setTodos((prev) => [...prev, { id: Date.now(), ...title }]);
   };
   const updateTodo = (id, title) => {
     setTodos((prev) =>
-      prev.map((prevtodo) => (prevtodo.id === id ? { ...prevtodo, ...title } : prevtodo))
+      prev.map((prevtodo) =>
+        prevtodo.id === id ? { ...prevtodo, ...title } : prevtodo
+      )
     ); // This is an if and else condition for the update to find the id and update the title
   };
   const deleteTodo = (id) => {
@@ -18,17 +21,30 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) =>
       prev.map((prevtodo) =>
-        prevtodo.id === id ? { ...prevtodo, completed: !prevtodo.completed } : prevtodo
+        prevtodo.id === id
+          ? { ...prevtodo, completed: !prevtodo.completed }
+          : prevtodo
       )
     ); // This is an if and else condition to toggle the complete
-
-    };
+  };
+  useEffect(() => {
+    const localTodos = JSON.parse(localStorage.getItem("todos"));
+    if (localTodos && localTodos.length > 0) {
+      setTodos(localTodos);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
-    <TodoProvider value={{todos,addtodo, updateTodo, deleteTodo, toggleComplete}}>
+    <TodoProvider
+      value={{ todos, addtodo, updateTodo, deleteTodo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+            {/*Manage Your Todos*/}
+            <TodoForm/>
           </h1>
           <div className="mb-4">{/* Todo form goes here */}</div>
           <div className="flex flex-wrap gap-y-3">
---
+++ constext_api_with_local_Storage/src/Components/TodoForm.jsx
@@ -0,0 +1,34 @@
+function TodoForm() {
+  const [title, setTitle] = useState("");
+  const {addtodo} = useTodoContext();
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!title) return;
+    addTodo({
+      title,
+      // / This is the newer way of writing title:title because the key and value are
+      //  the same this is the shorter way of writing it./
+      completed: false,
+    });
+    setTitle("");
+  };
+  return (
+    <form onSubmit={addTodo} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
