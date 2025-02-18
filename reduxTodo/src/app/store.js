51149b590bddc9fd9983ec4adac50041ee6810a9
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2025-02-19 01:05:20 +0530

    come back

--- reduxTodo/src/app/store.js
+++ reduxTodo/src/app/store.js
@@ -1,3 +1,6 @@
-import {configureStore} from '@reduxjs/toolkit';
-
-export const store = configureStore({});
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/Todo/TodoSlice';
+// import the reducer from the slice file
+export const store = configureStore({
+    reducer: todoReducer
+});
--- reduxTodo/src/features/Todo/TodoSlice.js
+++ reduxTodo/src/features/Todo/TodoSlice.js
@@ -1,25 +1,42 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+// nanoid is use to create unique ID for the todo
+// createSlice is used to create a slice of the state
 
 const initialState = {
     todos: [
         {
             id: '1',
-            text: "hello"
+            text: "hello",
+            completed: false
         }//This is how we can add a todo in the initial state
-        //this is the initial state of the todo
+        //this is the initial state of the Store 
     ]
 }
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
-        addtodo: (state, action) => {
+        addTodo: (state, action) => {
             //action and state are the two parameters that we should pass to the reducers
             const todo = {
                 id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo);
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload.id)
+            todo.text = action.payload.text;
+        },
+        toggleTodo: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload)
+            todo.completed = !todo.completed
         }
     }
 });
+
+export const { addTodo, deleteTodo, updateTodo, toggleTodo } = todoSlice.actions;
+export default todoSlice.reducer;
