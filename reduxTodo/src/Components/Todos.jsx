2d472e5aaec55ff073bbf381776d38fc5d448916
Aakash Yadav <77094404+Aakashh-yadav@users.noreply.github.com>
2025-02-20 01:09:36 +0530

    redux day 2

---
+++ reduxTodo/src/Components/Todos.jsx
@@ -0,0 +1,12 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTodo } from '../features/Todo/TodoSlice'; // import the removeTodo action creator 
+
+function RemoveTodo() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RemoveTodo
--- reduxTodo/src/app/store.js
+++ reduxTodo/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/Todo/TodoSlice';
+
 // import the reducer from the slice file
 export const store = configureStore({
     reducer: todoReducer
